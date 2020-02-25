2993ff5a4b1484bbfa1bfbd31d6d96f330485ce5
caohong <568034700@qq.com>
2020-02-26 01:01:54 +0800

    Merge branch 'master' of https://github.com/568034700/repo7
    # Conflicts:
    #	src/main/java/cn/itcast/controller/HelloController.java

--- src/main/java/cn/itcast/controller/HelloController.java
+++ src/main/java/cn/itcast/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author caohong 44444
+ * @author caohongv
  * @program springmvc_day01
  * @description
  * @create 2020-02-22 15:38
