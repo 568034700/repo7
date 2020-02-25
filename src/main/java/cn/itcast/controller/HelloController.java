138f7d5635ec014a218d48a28d85a12b012ef7ad
568034700 <45836742+568034700@users.noreply.github.com>
2020-02-26 00:20:18 +0800

    123

--- src/main/java/cn/itcast/controller/HelloController.java
+++ src/main/java/cn/itcast/controller/HelloController.java
@@ -10,13 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @create 2020-02-22 15:38
  */
 @Controller
-/**
- * @Description        
- * @param null	
- * @return 
- * @author caohong
- * @date 2020/2/25 13:21
- */
 public class HelloController {
     @RequestMapping(path = "/hello")
     public String sayHello() {
